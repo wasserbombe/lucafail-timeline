af8e30da2134575e1c929a8cf34c9d6a188b77c4
Sebastian Fuhrmann <sebastian.fuhrmann@rz-fuhrmann.de>
2021-06-09 15:06:05 +0200

    move to AJAX, move data file

--- index.php
+++ index.php
@@ -4,34 +4,25 @@
         <meta charset="utf-8">
 
         <link href="css/main.css" rel="stylesheet">
+
+        <title>timeline.luca.fail</title>
+
+        <!--
+            WELCOME TO THE SOURCE CODE!
+
+            If you're interested in constributing to this list, feel free to add an item on the link list of http://edit.luca.fail/ or 
+            directly at https://github.com/wasserbombe/lucafail-timeline
+
+            If you're interested in pulling our data automatically/regularly in a machine-readable format, feel free to use 
+            https://timeline.luca.fail/data/timeline_data.json as endpoint URL. But please note that the data format / data URL can change 
+            without any notice at any time, especially in the first days. You can leave us a quick note at dataaccess@luca.fail so that we 
+            know you're consuming the data and are able to notify you on major changes. 
+        -->
     </head>
     <body>
-        <div class="timeline">
-        <?php
-            $dataset = json_decode(file_get_contents(__DIR__.'/timeline_data.json'), true);
+        <div class="timeline"></div>
 
-            $toggle = true; 
-            foreach ($dataset as $data){
-                $toggle = !$toggle; 
-                echo '<div class="container '.($toggle?'left':'right').'">';
-                echo '  <div class="content">';
-                echo '    <span class="subtitle">'.$data["date"].'</span>';
-                echo '    <h2 class="title">'.$data["title"].'</h2>';
-                if (isset($data["text"])){
-                    echo '    <p>'.$data["text"].'</p>';
-                }
-                if (isset($data["links"]) && sizeof($data["links"])){
-                    echo '<b>Weiterführende Links:</b>';
-                    echo '<ul>';
-                    foreach ($data["links"] as $link){
-                        echo '<li><a href="'.$link["url"].'">'.$link["text"].'</a></li>';
-                    }
-                    echo '</ul>';
-                }
-                echo '  </div>';
-                echo '</div>';
-            }
-        ?>
-        </div>
+        <script src="/js/jquery.js"></script>
+        <script src="/js/main.js"></script>
     </body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,42 @@
+(function(){
+    $.ajax({
+        url: "/data/timeline_data.json",
+        dataType: 'json',
+        success: (data) => {
+            data.forEach((e, i) => {
+                $div = $("<div>").addClass("container").addClass((i%2 == 0)?"left":"right");
+
+                $content = $("<div>").addClass("content");
+
+                $subtitle = $("<span>").addClass("subtitle").html(e.date);
+                $content.append($subtitle);
+
+                $title = $("<h2>").addClass("title").html(e.title);
+                $content.append($title);
+
+                if (e.text){
+                    $text = $("<div>").html(e.text);
+                    $content.append($text);                    
+                }
+
+                if (e.links && e.links.length){
+                    $linklist = $("<ul>");
+                    e.links.forEach((link,i) => {
+                        $li = $("<li>");
+                        $a = $("<a>").attr("href", link.url).text(link.text);
+                        $li.append($a);
+                        $linklist.append($li);
+                    });
+                    
+                    $linkarea = $("<div>").addClass("linkarea").html('<b>Weiterführende Links:</b>').append($linklist);
+
+                    $content.append($linkarea);                    
+                }
+
+                $div.append($content);
+
+                $(".timeline").append($div);
+            });
+        }
+    });
+})(); 
