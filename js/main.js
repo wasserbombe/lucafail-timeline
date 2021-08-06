1e7d0b950c60a0503b8e38521a28988741360dce
Sebastian Fuhrmann <sebastian.fuhrmann@rz-fuhrmann.de>
2021-08-06 13:58:55 +0200

    test: add map

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 (function(){
+    var mymap = L.map('map_container').setView([51.505, -0.09], 13); 
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+
+    L.marker([51.5, -0.09]).addTo(mymap)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
     var typeFriendlyNames = {
         "general": "Allgemein",
         "doc": "Dokument/Paper/Veröffentlichung",
